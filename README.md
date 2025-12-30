76f604d31e6f9f9eb2800d7ee26ee0b019326317
Constantine Theocharis <cthe@mailbox.org>
2025-12-30 14:19:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## erasure-impl
+
+Toy implementation of type theory with erasure.
+
+To keep this self-contained and understandable, I have based it on András Kovács' [`elaboration-zoo/04-implicit-args`](https://github.com/AndrasKovacs/elaboration-zoo/tree/master/04-implicit-args)
---
+++ src/Main.hs
@@ -0,0 +1,58 @@
+
+module Main where
+
+import Control.Exception
+import System.Environment
+import System.Exit
+
+import Common
+import Cxt
+import Errors
+import Evaluation
+import Metacontext
+import Parser
+import Pretty
+import Elaboration
+
+import qualified Presyntax as P
+
+--------------------------------------------------------------------------------
+
+helpMsg = unlines [
+  "usage: erasure-impl [--help|elab|nf|type]",
+  "  --help : display this message",
+  "  elab   : read & elaborate expression from stdin",
+  "  nf     : read & typecheck expression from stdin, print its normal form and type",
+  "  type   : read & typecheck expression from stdin, print its type"]
+
+mainWith :: IO [String] -> IO (P.Tm, String) -> IO ()
+mainWith getOpt getRaw = do
+
+  let elab = do
+        (t, file) <- getRaw
+        infer (emptyCxt (initialPos file)) t
+          `catch` \e -> displayError file e >> exitSuccess
+
+  reset
+  getOpt >>= \case
+    ["--help"] -> putStrLn helpMsg
+    ["nf"]   -> do
+      (t, a) <- elab
+      putStrLn $ showTm0 $ nf [] t
+      putStrLn "  :"
+      putStrLn $ showTm0 $ quote 0 a
+    ["type"] -> do
+      (t, a) <- elab
+      putStrLn $ showTm0 $ quote 0 a
+    ["elab"] -> do
+      (t, a) <- elab
+      displayMetas
+      putStrLn $ showTm0 t
+    _ -> putStrLn helpMsg
+
+main :: IO ()
+main = mainWith getArgs parseStdin
+
+-- | Run main with inputs as function arguments.
+main' :: String -> String -> IO ()
+main' mode src = mainWith (pure [mode]) ((,src) <$> parseString src)
